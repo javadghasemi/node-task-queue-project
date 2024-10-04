865c3868a4d0b7341ad54dbbd438728dbc3770c3
Javad Ghasemi <javad.ghasemi8080@gmail.com>
2024-10-04 17:18:15 +0330

    add taskController

---
+++ TaskSubmitationService/Controllers/TaskController.js
@@ -0,0 +1,11 @@
+export class TaskController {
+  getAll(req, res) {}
+
+  create(req, res) {}
+
+  status(req, res) {}
+
+  result(req, res) {}
+
+  cancel(req, res) {}
+}
--- TaskSubmitationService/routes.js
+++ TaskSubmitationService/routes.js
@@ -1,8 +1,15 @@
 import express from 'express';
 
+import {TaskController} from "./Controllers/TaskController.js";
+
 const router = express.Router();
 
-router.get('api/v1/tasks', (req, res) => {
-});
+const taskController = new TaskController();
+
+router.get('api/v1/tasks', taskController.getAll.bind(taskController));
+router.post('api/v1/tasks', taskController.create.bind(taskController));
+router.get('api/v1/tasks/:taskId', taskController.status.bind(taskController));
+router.get('api/v1/tasks/:taskId/result', taskController.result.bind(taskController));
+router.delete('api/v1/tasks/:taskId', taskController.cancel.bind(taskController));
 
 export const routes = router;
