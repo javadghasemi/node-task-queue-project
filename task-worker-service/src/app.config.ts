40f9208b94d9cd94983f3ae2063d98c1d39c85b2
Javad Ghasemi <javad.ghasemi8080@gmail.com>
2024-10-08 23:36:49 +0330

    add logger to task-worker-service

---
+++ task-worker-service/src/app.config.ts
@@ -0,0 +1,37 @@
+import { WinstonModuleOptions } from 'nest-winston';
+import * as winston from 'winston';
+
+const environment = process.env.NODE_ENV || 'development';
+
+export function createWinstonConfig(): WinstonModuleOptions {
+  const productionFormat: WinstonModuleOptions = {
+    level: 'warn',
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.ms(),
+          winston.format.errors({ stack: true }),
+          winston.format.json(),
+        ),
+      }),
+    ],
+  };
+
+  const developmentFormat: WinstonModuleOptions = {
+    transports: [
+      new winston.transports.Console({
+        level: 'debug',
+        format: winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+          winston.format.printf(({ timestamp, level, message, ...meta }) => {
+            return `${timestamp} [${level}] ${message} ${Object.keys(meta).length ? JSON.stringify(meta) : ''}`;
+          }),
+        ),
+      }),
+    ],
+  };
+
+  return environment === 'development' ? developmentFormat : productionFormat;
+}
--- task-worker-service/src/app.module.ts
+++ task-worker-service/src/app.module.ts
@@ -4,9 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TaskWorkerModule } from './task-worker/task-worker.module';
 import { join, resolve } from 'node:path';
 import { ClusteringService } from './clustering.service';
+import { WinstonModule } from 'nest-winston';
+import { createWinstonConfig } from './app.config';
 
 @Module({
   imports: [
+    WinstonModule.forRoot(createWinstonConfig()),
     ConfigModule.forRoot({
       envFilePath: resolve(join(__dirname, '..', '..', '.env')),
     }),
